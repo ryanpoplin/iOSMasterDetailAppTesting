486f8c21a1bd55ab983e9b8c0afbe724e3e0f601
Byrdann Fox <mobileupstate@gmail.com>
2015-01-26 12:44:26 -0500

    Playground testings...

---
+++ AACcessTTS/MyPlayground.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground" // asdfjkl;
+
+/* ... */
+
+var poplin: String
+poplin = "Parter & CTO @ ExcepApps, Inc"
+
+// hold the 'option' key and click on variables...
+var age = 24
+// cannot do this: age = "..."
+
+var age = 25; age = 26
+
+let pi = 3.14 // ...
+
+pi = 3.1415
+
+
