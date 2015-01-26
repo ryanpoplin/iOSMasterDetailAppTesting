8fb391add451e81b1c53f2738445d687dae3ea7a
Byrdann Fox <mobileupstate@gmail.com>
2015-01-26 16:04:40 -0500

    playground hound...

--- AACcessTTS/MyPlayground.playground/section-1.swift
+++ AACcessTTS/MyPlayground.playground/section-1.swift
@@ -1,22 +1,89 @@
-// Playground - noun: a place where people can play
+//// Playground - noun: a place where people can play
+//
+//import UIKit
+//// import Swift
+//
+//var str = "Hello, playground" // asdfjkl;
+//
+///* ... */
+//
+//var poplin: String
+//poplin = "Parter & CTO @ ExcepApps, Inc"
+//
+//// hold the 'option' key and click on variables...
+//var age = 24
+//// cannot do this: age = "..."
+//
+//age = 25; age = 26
+//
+//let pi = 3.14 // ...
+//
+//// pi = 3.1415
+//
+//var varPi = pi
+//
+//println("...")
+//
+//Int8.max
+//Int8.min
+//Int32.max
+//Int32.min
+//UInt32.max
+//
+//let decimal = 123
+//
+//let octal = 0o77
+//
+//let hex = 0x1234
+//
+//let binary = 0b1010
+//
+//let v = -1_234
+//let w = 12_34_56
+//
+//let a = 1.23
+//
+//let b: Float = 1.23
+//
+//let c = 1.23e2
+//
+//let d = 1.23e-1
+//
+//let e = 1.23E-1
+//
+//let f: Float = 0.123456789
+//let g: Double = 0.123456789
+//
+//let anotherA = 123
+//let anotherB = 0.456
+//
+//// tis an err...
+//// let c = anotherA + anotherB
+//
+//let anotherC = Double(anotherA) + anotherB
+//
+//let cgFloat: CGFloat = 123
+//let anothercgFloat: Double = 456
+//let cgCFloat = cgFloat + CGFloat(anothercgFloat)
+//
+//let accuCGFloat: CGFloat = 123
+//let accuDouble: Double = 456
+//
+//let accu = Double(accuCGFloat) + accuDouble
+//
+//var testraa = 1 + 23.6
 
-import UIKit
+let aa = Int.max
+let bb = 1
+let cc = aa &+ bb
 
-var str = "Hello, playground" // asdfjkl;
+let aaa = 4
+let bbb = aaa << 2
+let ccc = bbb >> 1
 
-/* ... */
+let aaaa = -4
+let bbbb = aaaa << 2
+let cccc = bbbb >> 1
 
-var poplin: String
-poplin = "Parter & CTO @ ExcepApps, Inc"
-
-// hold the 'option' key and click on variables...
-var age = 24
-// cannot do this: age = "..."
-
-var age = 25; age = 26
-
-let pi = 3.14 // ...
-
-pi = 3.1415
 
 
